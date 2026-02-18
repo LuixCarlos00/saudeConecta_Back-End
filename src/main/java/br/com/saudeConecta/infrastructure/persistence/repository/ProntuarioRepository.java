6f38c2191fab39a0b93af12130f4e1f3e92b21a9
LuixCarlos <107255451+LuixCarlos00@users.noreply.github.com>
2026-02-18 19:53:33 -0300

    SCV-13.9
    SCV-13.9 Padronização de de Relatorios

--- src/main/java/br/com/saudeConecta/infrastructure/persistence/repository/ProntuarioRepository.java
+++ src/main/java/br/com/saudeConecta/infrastructure/persistence/repository/ProntuarioRepository.java
@@ -2,6 +2,7 @@ package br.com.saudeConecta.infrastructure.persistence.repository;
 
 import br.com.saudeConecta.domain.prontuario.Prontuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,12 @@ public interface ProntuarioRepository extends JpaRepository<Prontuario, Long> {
     List<Prontuario> findByConsulta_Paciente_PaciCodigo(Long paciCodigo);
     
     List<Prontuario> findByProfissional_Id(Long profissionalId);
+    
+    @Query("SELECT p FROM Prontuario p " +
+           "LEFT JOIN FETCH p.profissional " +
+           "LEFT JOIN FETCH p.consulta c " +
+           "LEFT JOIN FETCH c.paciente " +
+           "LEFT JOIN FETCH c.formaPagamento " +
+           "WHERE p.consulta.id = :consultaId")
+    Prontuario findByConsulta_IdWithFetch(Long consultaId);
 }
